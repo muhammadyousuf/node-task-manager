41c3210235232f1f8b91459f35a51f4398679778
muhammad yousuf <muhammadyousuf327@gmail.com>
2019-11-24 14:20:21 +0500

    add object id

--- mongodb.js
+++ mongodb.js
@@ -1,8 +1,14 @@
-const mongodb = require("mongodb");
-const MongoClient = mongodb.MongoClient;
+const { MongoClient, ObjectID } = require("mongodb");
 
+const id = new ObjectID();
 const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "taske-manager";
+
+console.log(id);
+console.log(id.id);
+console.log(id.id.length);
+console.log(id.toHexString().length);
+console.log(id.getTimestamp());
 
 MongoClient.connect(
   connectionURL,
@@ -12,18 +18,19 @@ MongoClient.connect(
       return console.log("Unable to connect database");
     }
     const db = client.db(databaseName);
-    // db.collection("users").insertOne(
-    //   {
-    //     name: "Muhammad Yousuf",
-    //     age: 24
-    //   },
-    //   (error, result) => {
-    //     if (error) {
-    //       return console.log("Unable to insert user");
-    //     }
-    //     console.log(result.ops)
-    //   }
-    // );
+    db.collection("users").insertOne(
+      {
+        _id: id,
+        name: "Muhammad Yousuf",
+        age: 22
+      },
+      (error, result) => {
+        if (error) {
+          return console.log("Unable to insert user");
+        }
+        console.log(result.ops);
+      }
+    );
 
     // db.collection("users").insertMany([
     //   {
@@ -42,27 +49,27 @@ MongoClient.connect(
     //   }
     // );
 
-    db.collection("tasks").insertMany(
-      [
-        {
-          description: "Node JS Basic Video",
-          completed: false
-        },
-        {
-          description: "Hooks Learning",
-          completed: true
-        },
-        {
-          description: "Women App",
-          completed: false
-        }
-      ],
-      (error, result) => {
-        if (error) {
-          return console.log("Unable to insert task");
-        }
-        console.log(result.ops);
-      }
-    );
+    // db.collection("tasks").insertMany(
+    //   [
+    //     {
+    //       description: "Node JS Basic Video",
+    //       completed: false
+    //     },
+    //     {
+    //       description: "Hooks Learning",
+    //       completed: true
+    //     },
+    //     {
+    //       description: "Women App",
+    //       completed: false
+    //     }
+    //   ],
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unable to insert task");
+    //     }
+    //     console.log(result.ops);
+    //   }
+    // );
   }
 );
