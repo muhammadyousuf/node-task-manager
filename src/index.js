ee12bcf8a641d50a86f9b0cb7c4bbab33a8f5784
muhammad yousuf <muhammadyousuf327@gmail.com>
2019-12-06 00:15:31 +0500

    get single and all data

--- src/index.js
+++ src/index.js
@@ -23,6 +23,43 @@ app.post("/tasks", (req, res) => {
     .then(result => res.status(201).send(result))
     .catch(err => res.status(400).send(err));
 });
+
+app.get("/users", (req, res) => {
+  User.find({})
+    .then(users => res.status(200).send(users))
+    .catch(err => res.status(500).send(err));
+});
+
+app.get("/tasks", (req, res) => {
+  Task.find({})
+    .then(tasks => res.status(200).send(tasks))
+    .catch(err => res.status(500).send(err));
+});
+
+app.get("/users/:id", (req, res) => {
+  const _id = req.params.id;
+  User.findById(_id)
+    .then(user => {
+      if (!user) {
+        return res.status(404).send([]);
+      }
+      res.status(200).send(user);
+    })
+    .catch(err => res.status(500).send(err));
+});
+
+app.get("/tasks/:id", (req, res) => {
+  const _id = req.params.id;
+  Task.findById(_id)
+    .then(task => {
+      if (!task) {
+        return res.status(404).send([]);
+      }
+      res.status(200).send(task);
+    })
+    .catch(err => res.status(500).send(err));
+});
+
 app.listen(port, () => {
   console.log("Server is up an running on port " + port);
 });
