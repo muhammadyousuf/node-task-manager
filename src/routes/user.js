7ac174bc8775369448f4a126b9b36035dc2c6e17
muhammad yousuf <muhammadyousuf327@gmail.com>
2019-12-08 19:02:56 +0500

    add seperate routes

---
+++ src/routes/user.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const User = require("../models/user");
+const router = new express.Router();
+
+router.post("/users", async (req, res) => {
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+router.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).send(users);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+router.get("/users/:id", async (req, res) => {
+    const _id = req.params.id;
+    try {
+      const user = await User.findById(_id);
+      if (!user) {
+        return res.status(404).send([]);
+      }
+      res.status(200).send(user);
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  });
+
+  router.patch("/users/:id", async (req, res) => {
+    const _id = req.params.id;
+    const updates = Object.keys(req.body);
+    const allowUpdates = ["name", "email", "age", "password"];
+    const isValidOperator = updates.every(update =>
+      allowUpdates.includes(update)
+    );
+    if (!isValidOperator) {
+      return res.status(400).send({ error: "Invalid Updates!" });
+    }
+    try {
+      const user = await User.findByIdAndUpdate(_id, req.body, {
+        new: true,
+        runValidators: true
+      });
+      if (!user) {
+        return res.status(404).send([]);
+      }
+      res.status(200).send(user);
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  });
+
+  router.delete("/users/:id", async (req, res) => {
+    const _id = req.params.id;
+    try {
+      const user = await User.findByIdAndDelete(_id);
+      if (!user) {
+        return res.status(404).send([]);
+      }
+      res.status(200).send(user);
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  });
+  
+
+module.exports = router;
