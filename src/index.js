eab63a939303e02674abe9b8b7553c1bab617076
muhammad yousuf <muhammadyousuf327@gmail.com>
2019-12-08 19:24:27 +0500

    add bycrpt password

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("./db/mongoose");
 const userRoutes = require("./routes/user");
 const taskRoutes = require("./routes/task");
+const bcrypt = require("bcryptjs");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,3 +13,13 @@ app.use(taskRoutes);
 app.listen(port, () => {
   console.log("Server is up an running on port " + port);
 });
+
+const myFunction = async () => {
+  const password = "yousufPass";
+  const passwordHash = await bcrypt.hash(password, 8);
+  const match = await bcrypt.compare("yousufPass", passwordHash)
+  console.log(password);
+  console.log(passwordHash);
+  console.log(match)
+};
+myFunction();
