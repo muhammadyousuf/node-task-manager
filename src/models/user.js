2f130642675b38580e33fb18ed63fef4b59fdb96
muhammad yousuf <muhammadyousuf327@gmail.com>
2019-12-22 20:11:04 +0500

    login user route

--- src/models/user.js
+++ src/models/user.js
@@ -10,6 +10,7 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
+    unique: true,
     trim: true,
     lowercase: true,
     validate(value) {
@@ -39,6 +40,18 @@ const userSchema = new mongoose.Schema({
     }
   }
 });
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error("Unable to Login");
+  }
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw new Error("Unable to Login");
+  }
+  return user;
+};
 
 userSchema.pre("save", async function(next) {
   const user = this;
--- src/routes/user.js
+++ src/routes/user.js
@@ -12,6 +12,20 @@ router.post("/users", async (req, res) => {
   }
 });
 
+router.post("/users/login", async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    res.status(200).send(user);
+  } catch (e) {
+    res.status(400).json({
+      error: "Login Failed"
+    });
+  }
+});
+
 router.get("/users", async (req, res) => {
   try {
     const users = await User.find({});
@@ -50,9 +64,9 @@ router.patch("/users/:id", async (req, res) => {
     //   runValidators: true
     // });
     const user = await User.findById(_id);
-   
+
     updates.forEach(update => (user[update] = req.body[update]));
-    
+
     if (!user) {
       return res.status(404).send([]);
     }
