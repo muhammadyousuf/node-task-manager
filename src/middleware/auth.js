d5aa7a789662e732739a80520ae4fd3d895767eb
muhammad yousuf <muhammadyousuf327@gmail.com>
2019-12-23 21:03:33 +0500

    implement logout functionality

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,10 +4,14 @@ const auth = async (req, res, next) => {
   try {
     const token = req.header("Authorization").replace("Bearer ", "");
     const decoded = jwt.verify(token, "secret");
-    const user = await User.findOne({ _id: decoded._id, "tokens.token": token });
+    const user = await User.findOne({
+      _id: decoded._id,
+      "tokens.token": token
+    });
     if (!user) {
       throw new Error();
     }
+    req.token = token;
     req.user = user;
     next();
   } catch (e) {
--- src/routes/user.js
+++ src/routes/user.js
@@ -28,6 +28,27 @@ router.post("/users/login", async (req, res) => {
     });
   }
 });
+router.post("/users/logout", auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter(token => {
+      return token.token != req.token;
+    });
+    await req.user.save();
+    res.send();
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+router.post("/users/logoutAll", auth, async (req, res) => {
+  try {
+    req.user.tokens = []
+    await req.user.save();
+    res.send();
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
 
 router.get("/users", async (req, res) => {
   try {
@@ -39,8 +60,7 @@ router.get("/users", async (req, res) => {
 });
 
 router.get("/users/me", auth, async (req, res) => {
-  
- res.send(req.user);
+  res.send(req.user);
 });
 
 router.get("/users/:id", async (req, res) => {
