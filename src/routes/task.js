df389cc8a8dfd1b745054338d23f3d4b7a94162e
muhammad yousuf <muhammadyousuf327@gmail.com>
2019-12-29 14:01:49 +0500

    add sorting data

--- src/routes/task.js
+++ src/routes/task.js
@@ -18,8 +18,14 @@ router.post("/tasks", auth, async (req, res) => {
 
 router.get("/tasks", auth, async (req, res) => {
   const match = {};
+  const sort = {};
   if (req.query.completed) {
     match.completed = req.query.completed === "true";
+  }
+
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split(":");
+    sort[parts[0]] = parts[1] === "desc" ? -1 : 1;
   }
   try {
     await req.user
@@ -28,7 +34,8 @@ router.get("/tasks", auth, async (req, res) => {
         match,
         options: {
           limit: parseInt(req.query.limit),
-          skip: parseInt(req.query.skip)
+          skip: parseInt(req.query.skip),
+          sort
         }
       })
       .execPopulate();
