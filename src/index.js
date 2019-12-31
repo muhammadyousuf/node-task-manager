fdeb5998b30715b6d0737002fcdccd1566829c3c
muhammad yousuf <muhammadyousuf327@gmail.com>
2019-12-31 20:44:50 +0500

    handle validation on upload file

--- src/index.js
+++ src/index.js
@@ -10,7 +10,16 @@ const port = process.env.PORT || 5000;
 
 const multer = require("multer");
 const upload = multer({
-  dest: "images"
+  dest: "images",
+  limits: {
+    fileSize: 5000000
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(pdf|doc|docx|png|jpg|jpeg)$/)) {
+      return cb(new Error("Please upload a valid file"));
+    }
+    cb(undefined, true);
+  }
 });
 app.post("/upload", upload.single("upload"), (req, res) => {
   res.send();
