2a8952f793844e0e378c5193459450b535f83997
muhammad yousuf <muhammadyousuf327@gmail.com>
2020-01-04 14:16:34 +0500

    send and cancel emails

--- src/emails/account.js
+++ src/emails/account.js
@@ -4,9 +4,25 @@ const sendgridAPIKey =
 
 sgMail.setApiKey(sendgridAPIKey);
 
-sgMail.send({
-  to: "yousufbashir341@gmail.com",
-  from: "muhammadyousuf327@gmail.com",
-  subject: "Sending with Twilio SendGrid is Fun",
-  text: "and easy to do anywhere, even with Node.js"
-});
+const sendWelcomeEmail = (email, name) => {
+  sgMail.send({
+    to: email,
+    from: "muhammadyousuf327@gmail.com",
+    subject: "Thanks for joininig in!.",
+    text: `Welcome to the app, ${name} Let me know how you get along with the app.`
+  });
+};
+
+const sendCancelationEmail = (email, name) => {
+  sgMail.send({
+    to: email,
+    from: "muhammadyousuf327@gmail.com",
+    subject: "Sorry to see you go!.",
+    text: `Goodbye, ${name} I hope to see you back sometime soon.`
+  });
+};
+
+module.exports = {
+  sendWelcomeEmail,
+  sendCancelationEmail
+};
