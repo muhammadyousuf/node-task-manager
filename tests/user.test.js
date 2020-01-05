a4be6b0f29dc7ed6d2ff7d24016dc1452eadf5c4
muhammad yousuf <muhammadyousuf327@gmail.com>
2020-01-05 18:13:24 +0500

    add user login test case

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,5 +1,17 @@
 const request = require("supertest");
 const app = require("../src/app");
+const User = require("../src/models/user");
+
+const userOne = {
+  name: "Muhammad Rafae",
+  email: "rafaekhatri11@gmail.com",
+  password: "HeyWhat1!"
+};
+
+beforeEach(async () => {
+  await User.deleteMany();
+  await new User(userOne).save();
+});
 
 test("should signup new user", async () => {
   await request(app)
@@ -11,3 +23,24 @@ test("should signup new user", async () => {
     })
     .expect(201);
 });
+
+test("should login existing user", async () => {
+  await request(app)
+    .post("/users/login")
+    .send({
+      email: userOne.email,
+      password: userOne.password
+    })
+    .expect(200);
+});
+
+
+test("should not login nonexisting user", async () => {
+    await request(app)
+      .post("/users/login")
+      .send({
+        email: userOne.email,
+        password: 'sh43ajs'
+      })
+      .expect(400);
+  });
